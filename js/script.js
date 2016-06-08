a9669317ff580a58b01a044dfafb62c82b5d4210
Betsy Gottesman <eightamps@gmail.com>
2016-06-07 22:58:34 -0700

    started writing angular routing and controller

--- js/script.js
+++ js/script.js
@@ -1,7 +1,43 @@
-$(function($){
-    //doc ready
+var recipeMate = angular.module('recipeMate', [])
 
+.controller('MainController', function(){
+    this.foodList = [
+    {
+        name: 'Hamburger',
+        category: 'Sandwiches',
+        servingSize: '100g',
+        calories: 270,
+        carbs: 80,
+        fat: 20,
+        protein: 25,
+        sodium: 2300,
+        fiber: 6
+    },
+    {
+            name: 'Avocado Salad',
+            category: 'Salads',
+            servingSize: '1 cup',
+            calories: 80,
+            carbs: 0,
+            fat: 0,
+            protein: 0,
+            sodium: 0,
+            fiber: 1
+    },
+    {
+            name: 'Penne Puttanesca',
+            category: 'Pasta',
+            servingSize: '1 cup',
+            calories: 350,
+            carbs: 95,
+            fat: 8,
+            protein: 15,
+            sodium: 2500,
+            fiber: 3
+    }
+    ];
 
-
-    
+    this.addData = function(object){
+      this.foodList.push(object);
+    };
 });
