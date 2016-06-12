9c6957c6cc23a8d944814f0302d7ea1589c1506d
Betsy Gottesman <eightamps@gmail.com>
2016-06-11 23:38:10 -0700

    used ng-repeat on food table and renamed js file to app.js

--- js/app.js
+++ js/app.js
@@ -5,7 +5,8 @@ app.config(function($routeProvider){
     $routeProvider
         .when('/', {
             templateUrl: 'nutrition.html',
-            controller: "mainController"
+            controller: "mainController",
+            controllerAs: "mc"
         })
         .when('/database', {
             templateUrl: 'database.html'
@@ -14,25 +15,25 @@ app.config(function($routeProvider){
             templateUrl: 'dashboard.html'
         })
         .otherwise({
-           redirectTo: '/'
+            redirectTo: '/'
         });
 
 });
 
 app.controller('mainController', function(){
     this.foodList = [
-    {
-        name: 'Hamburger',
-        category: 'Sandwiches',
-        servingSize: '100g',
-        calories: 270,
-        carbs: 80,
-        fat: 20,
-        protein: 25,
-        sodium: 2300,
-        fiber: 6
-    },
-    {
+        {
+            name: 'Hamburger',
+            category: 'Sandwiches',
+            servingSize: '100g',
+            calories: 270,
+            carbs: 80,
+            fat: 20,
+            protein: 25,
+            sodium: 2300,
+            fiber: 6
+        },
+        {
             name: 'Avocado Salad',
             category: 'Salads',
             servingSize: '1 cup',
@@ -42,8 +43,8 @@ app.controller('mainController', function(){
             protein: 0,
             sodium: 0,
             fiber: 1
-    },
-    {
+        },
+        {
             name: 'Penne Puttanesca',
             category: 'Pasta',
             servingSize: '1 cup',
@@ -53,10 +54,21 @@ app.controller('mainController', function(){
             protein: 15,
             sodium: 2500,
             fiber: 3
-    }
+        },
+        {
+            name: 'Tuna Sushi',
+            category: 'Fish',
+            servingSize: '1 piece',
+            calories: 50,
+            carbs: 10,
+            fat: 2,
+            protein: 2,
+            sodium: 200,
+            fiber: 0
+        }
     ];
 
     this.addData = function(object){
-      this.foodList.push(object);
+        this.foodList.push(object);
     };
 });
